6a0309ca52fdd384f51a7d0c27782cdd23680f6e
Orlov <alexx4456@yandex.ru>
2023-01-25 16:02:09 +0300

    правка маппера

--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -1,10 +1,8 @@
 package ru.practicum.shareit.user;
 
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Component;
 import ru.practicum.shareit.user.dto.UserDto;
 
-@Component
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toUserDto(User model);
