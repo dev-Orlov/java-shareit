a6917bf81745b83a29d70a204a7350146d79cb28
Orlov <alexx4456@yandex.ru>
2023-01-25 14:32:17 +0300

    убраны лишние аннотации @RequestBody

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -15,13 +15,11 @@ public class ItemController {
     private final ItemService itemService;
     private static final String OWNER = "X-Sharer-User-Id";
 
-    @ResponseBody
     @PostMapping
     public ItemDto create(@Valid @RequestBody ItemDto itemDto, @RequestHeader(OWNER) Long ownerId) {
         return itemService.create(itemDto, ownerId);
     }
 
-    @ResponseBody
     @PatchMapping("/{itemId}")
     public ItemDto update(@RequestBody ItemDto itemDto, @PathVariable Long itemId,
                           @RequestHeader(OWNER) Long ownerId) {
