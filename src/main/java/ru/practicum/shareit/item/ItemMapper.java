37bd91196dab0c53576d5569971d15111247cc1b
Orlov <alexx4456@yandex.ru>
2023-01-25 15:54:19 +0300

    правки по codestyle 3

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -1,9 +1,11 @@
 package ru.practicum.shareit.item;
 
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
     ItemDto toItemDto(Item model);
--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -1,8 +1,10 @@
 package ru.practicum.shareit.user;
 
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.user.dto.UserDto;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toUserDto(User model);
