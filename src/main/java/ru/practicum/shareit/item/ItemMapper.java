fd12ab01576f564a2921fa332e31009f1f924bf9
Orlov <alexx4456@yandex.ru>
2023-01-25 16:26:11 +0300

    Revert "правки по codestyle 2"
    This reverts commit b3c117174fdbf486067ee6b75adbf01cd84a816b.

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -6,6 +6,7 @@ import ru.practicum.shareit.item.model.Item;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
+
     ItemDto toItemDto(Item model);
 
     Item toItem(ItemDto dto);
